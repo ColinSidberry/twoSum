cfbffea9cbaf43ae3f161902af0c75f43d03e575
Colin <39615668+ColinSidberry@users.noreply.github.com>
2024-09-01 17:17:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const twoSum = (nums, target) => {
+};
+
+// Example 1:
+console.log(twoSum([2, 7, 11, 15], 9), "[0,1]")
+
+// Example 2:
+twoSum([3, 2, 4], 6); // [1,2]
+
+// Example 3:
+twoSum([3, 4], 6); // [0,1]
